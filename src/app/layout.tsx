f6955c3bf02a73d9a02119888f13b4ef1c3bb14c
Ahmed Ghonim <35805998+ahmedghonim@users.noreply.github.com>
2023-11-06 23:42:56 +0200

    fix: 🎨 asd

---
+++ src/app/layout.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import "@styles/globals.css";
+import { languages } from "@/i18n/settings";
+
+export async function generateStaticParams() {
+  return languages.map((lng) => ({ lng }));
+}
+
+export default function RootLayout({
+  children,
+  params: { lang },
+}: {
+  children: React.ReactNode;
+  params: { lang: any };
+}) {
+  return (
+    <html lang={lang} dir={lang === "ar" ? "rtl" : "ltr"}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
+        <title>
+          مكتب محاسبة ومراجعة للاستشارات الضريبية والقانونية M1 GROUP
+        </title>
+        <meta
+          name="keywords"
+          content="محاسبون قانونيون و مراجعون و خدمات ضرائب, محاسب ضرائب, خبير ضرائب ، تأسيس مؤسسه ، مكتب محاسب قانوني ، فض منازعات, خبير مثمن ، محاسب ضرائب ، مستشار قانوني ، مراجعه حسابات، مقيم عقارات ،مراقب حسابات ، شهاده محاسب قانوني ،مراجع حسابات ، شهاده إدارة محاسب"
+        />
+        <meta
+          name="description"
+          content="مكتب محمد رزق و شركاه للمحاسبه القانونيه و الضرائب و كافه المهام الورقيه والاجراءات القانونيه مع المتابعه المستمرة وتخليص الاجراءات الحكوميه لتاسيس شركتك                     ."
+        />
+        <meta
+          name="robots"
+          content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large"
+        />
+        <meta property="og:locale" content="ar_AR,en_US" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="مكتب محاسبة ومراجعة للاستشارات الضريبية والقانونية M1 GROUP"
+        />
+        <meta
+          property="og:description"
+          content="تعتبر مجموعه m1 محمد رزق للمحاسبه القانونيه ، مكنب محاسبهو مراجعه رائد في مجال العمل المحاسبي و الاعمال الضريبيه تمتاز بخبره اكثر من عشره اعوام في مجال المحاسبه و المراجعه."
+        />
+        <meta property="og:url" content="https://www.m1-group.com.eg/" />
+        <meta
+          property="og:site_name"
+          content="مكتب محاسبة و ضرائب و تأسيس شركات M1 GROUP"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content="مكتب محاسبة ومراجعة M1 GROUP للاستشارات الضريبية والقانونية"
+        />
+        <meta name="twitter:label1" content="Written by Ahmed Ghonim" />
+
+        <meta
+          name="google-site-verification"
+          content="O3e44RBjvHwhcdVwBLbuKLh4qyYcX_d9lOx0AjHWtbM"
+        />
+      </head>
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,54 @@
+import HomeIntro from "./_components/public-page/home/intro-section";
+import Statistics from "./_components/public-page/home/statistics";
+import AboutUS from "./_components/public-page/home/about-us";
+import WhyUs from "./_components/public-page/home/why-us";
+import Services from "./_components/public-page/services";
+import Link from "next/link";
+import Clients from "./_components/public-page/home/clients";
+import { translation } from "@/i18n";
+import { getHeroData } from "./[lang]/(admin)/admin/api-calls";
+import { getInoNumbersData } from "./[lang]/(admin)/admin/our-industries/api-calls";
+import { getAboutData } from "./[lang]/(admin)/admin/about-us/api-calls";
+import { getServiceData } from "./[lang]/(admin)/admin/our-services/api-calls";
+import { getClientData } from "./[lang]/(admin)/admin/our-client/api-calls";
+import { HeroTypes } from "./[lang]/(admin)/admin/page";
+import { NumbersInfo } from "@prisma/client";
+import { AboutTypes } from "./[lang]/(admin)/admin/about-us/page";
+
+export default async function Home({
+  params: { lang },
+}: {
+  params: { lang: "en" | "ar" };
+}) {
+  const { t } = await translation(lang, "common");
+  const heroData = (await getHeroData()) as HeroTypes;
+  const inoNumbersData = (await getInoNumbersData()) as NumbersInfo;
+  const aboutData = (await getAboutData()) as AboutTypes;
+  const ServiceData = await getServiceData();
+  const clientData = await getClientData();
+  return (
+    <div>
+      <section className="w-full">
+        <HomeIntro data={heroData} />
+        <Statistics data={inoNumbersData} />
+        <AboutUS lang={lang} data={aboutData} />
+        <WhyUs />
+        <Services
+          className="bg-[#F2E4BF] md:pt-[70px] pt-10 md:pb-[108px] pb-10 px-6 rounded-[12px]"
+          data={ServiceData as any}
+          lang={lang}
+        >
+          <div className="flex justify-end">
+            <Link
+              href={`${lang}/our-services`}
+              className="bg-dark-100 !text-[16px] py-3 px-5 text-white hover:opacity-60 duration-200 rounded font-Lato font-bold mt-6"
+            >
+              {t("read_more")}
+            </Link>
+          </div>
+        </Services>
+        <Clients data={clientData} />
+      </section>
+    </div>
+  );
+}
